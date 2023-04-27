95266b29716a3e47f094ae8db086eb525a806a52
ViktoriiaKN <113258083+ViktoriiaKN@users.noreply.github.com>
2023-04-27 19:25:04 +0000

    Deploying to gh-pages from @ ViktoriiaKN/goit-js-hw-10@ea91817b03f92c86a12d0364a403caa62934e5ea 🚀

---
+++ index.004ab460.js
@@ -0,0 +1,2 @@
+fetch("https://restcountries.com/v3.1/all").then((function(n){return n.json()})).then((function(n){return console.log(n)}));
+//# sourceMappingURL=index.004ab460.js.map
---
+++ index.ff3a6567.js
@@ -0,0 +1,2 @@
+fetch("https://restcountries.com/v3.1/all").then((t=>t.json())).then((t=>console.log(t)));
+//# sourceMappingURL=index.ff3a6567.js.map
