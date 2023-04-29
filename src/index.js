19debb061a270e37f2ae8de17058c234790d0f83
ViktoriiaKN <113258083+ViktoriiaKN@users.noreply.github.com>
2023-04-29 22:24:03 +0200

    Перейменувала

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './css/styles.css';
-import { fetchCountries } from './api/fetch';
+import { fetchCountries } from './api/fetchCountries';
 import { debounce } from 'lodash.debounce';
 import {Notiflix} from 'notiflix';
 
