974938d54f812bb10684bb5b42c326a9a86c7a2c
ViktoriiaKN <113258083+ViktoriiaKN@users.noreply.github.com>
2023-04-29 18:04:41 +0200

    Функція інпуту

--- src/index.js
+++ src/index.js
@@ -6,5 +6,20 @@ import {Notiflix} from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 
+const refs = {
+    input: document.getElementById('search-box'),
+    countryList: document.querySelector('.country-list'),
+    countryInfo: document.querySelector('.country-info'),
+};
 
+refs.input.addEventListener('input',)
 
+function searchCountry(e) {
+    e.preventDefault();
+    const valueInput = e.target.value.trim();
+    if (!valueInput) {
+        resetMarkup(refs.countryList);
+        resetMarkup(refs.countryInfo);
+        return;
+    }
+}
